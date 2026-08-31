4f6dd4a660ca75d20d399ce59583ddc27e7b0909
amandwandwe <devzwide@gmail.com>
2026-08-31 13:20:32 +0200

    tailored it into retail platform structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./main.css";
+import router from "./router.jsx"
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
---
+++ src/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1 className="text-4xl font-bold text-gray-900 tracking-tight mb-4">Hello...</h1>
+    }
+]);
+
+export default router;
